a08b1daf80c75d5d9820ce77c609d7e674543734
Jimmy Tang <jimmy.tang@outright.com>
2011-10-30 00:26:12 -0700

    just the yozio files now, removed the game.

--- Yozio/Yozio.h
+++ Yozio/Yozio.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Timer.h"
 
 @interface Yozio : NSObject
 {
---
+++ Yozio/lib/Timer.m
@@ -0,0 +1,41 @@
+//
+//  Timer.m
+//  GrenadeGame
+//
+//  Copyright 2011 UC Berkeley. All rights reserved.
+//
+
+#import "Timer.h"
+
+@implementation Timer
+
+- (id) init {
+  self = [super init];
+  if (self != nil) {
+    start = nil;
+    end = nil;
+  }
+  return self;
+}
+
+- (void) startTimer {
+  start = [NSDate date];
+}
+
+- (void) stopTimer {
+  end = [NSDate date];
+}
+
+- (double) timeElapsedInSeconds {
+  return [end timeIntervalSinceDate:start];
+}
+
+- (double) timeElapsedInMilliseconds {
+  return [self timeElapsedInSeconds] * 1000.0f;
+}
+
+- (double) timeElapsedInMinutes {
+  return [self timeElapsedInSeconds] / 60.0f;
+}
+
+@end
