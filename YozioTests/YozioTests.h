22bba0738cecd7c0f9d86a411d8faf77fdc17b7d
J Tang <jimmytang@gmail.com>
2011-11-05 00:03:44 -0700

    - Added unit testing framework.

---
+++ YozioTests/YozioTests.h
@@ -0,0 +1,13 @@
+//
+//  YozioTests.h
+//  YozioTests
+//
+//  Created by Dounan Shi on 11/4/11.
+//  Copyright (c) 2011 University of California at Berkeley. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface YozioTests : SenTestCase
+
+@end
---
+++ YozioTests/YozioTests.m
@@ -0,0 +1,32 @@
+//
+//  YozioTests.m
+//  YozioTests
+//
+//  Created by Dounan Shi on 11/4/11.
+//  Copyright (c) 2011 University of California at Berkeley. All rights reserved.
+//
+
+#import "YozioTests.h"
+
+@implementation YozioTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in YozioTests");
+}
+
+@end
