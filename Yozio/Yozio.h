dbe22720e18a3562399a3f5dc4b4a44647fcbc6e
Dounan Shi <dounan@gmail.com>
2012-08-23 17:20:37 -0700

    - Update Yozio.h documentation

--- Yozio/Yozio.h
+++ Yozio/Yozio.h
@@ -25,52 +25,60 @@
 + (void)configure:(NSString *)appKey secretKey:(NSString *)secretKey;
 
 /**
- * Configures Yozio with your user's username. This is used to provide a better display to your data. (Optional)
- 
- * @param userName  The application's username.
+ * Notify Yozio that your user logged in.
+ * This allows you to tie your data with Yozio's by user name.
+ *
+ * @param userName  The name of the user that just logged in.
  */
 + (void)userLoggedIn:(NSString *)userName;
 
 /**
- * Configures the Yozio SDK. Must be called when the app is initialized.
- * Connects to server to download configuration dictionary. Blocking
- *
+ * Initializes the Yozio SDK for experiments. Must be called when the app is initialized.
+ * Makes a blocking HTTP request to download the experiment configurations.
  */
 + (void)initializeExperiments;
 
 /**
- * Returns a configuration Object for key.
- * @param key      the key look up the Object by
- * @param defaultValue  default value in case the key isn’t found.
+ * Retrieve the String value for a given configuration key.
+ *
+ * @param key  The key of the value to retrieve. Must match a configuration key created online.
+ * @param defaultValue  The value to return if the key is not found.
+ * @return The configuration String value, or defaultValue if the key is not found.
  */
 + (NSString*)stringForKey:(NSString *)key defaultValue:(NSString *)defaultValue;
 
 /**
- * Returns a configuration Int for key.
- * @param key      the key look up the Int by
- * @param defaultValue  default value in case the key isn’t found.
+ * Retrieve the Int value for a given configuration key.
+ *
+ * @param key  The key of the value to retrieve. Must match a configuration key created online.
+ * @param defaultValue  The value to return if the key is not found.
+ * @return The configuration Int value, or defaultValue if the key is not found.
  */
 + (NSInteger)intForKey:(NSString *)key defaultValue:(NSInteger)defaultValue;
 
 /**
- * Retrieve the shortened url for a dynamic link. Returns the destinationUrl if it can't get a short url. (blocking)
+ * Retrieve the Yozio short url for a given linkName.
  *
- * @param linkName  the name of the link. Must match one of the link names created online.
- * @param destinationUrl  the url that the shortened url must redirect to.
+ * @param linkName  The name of the viral tracking link.
+ *                  Must match one of the link names created online.
+ * @param destinationUrl  The url that the shortened url will redirect to.
+ * @return The Yozio short URL for the linkName, or destinationUrl if there is an error.
  */
 + (NSString *)getUrl:(NSString *)linkName destinationUrl:(NSString *)destinationUrl;
 
 /**
- * Alert Yozio that a user has viewed a link.
- * To edit this linkName, go to the VIRAL section of the Yozio website to find the appropriate link name to track.
- * @param linkName  the name of the link. Must match one of the link names created online.
+ * Notify Yozio that a user has viewed a link.
+ *
+ * @param linkName  The name of the viral tracking link.
+ *                  Must match one of the link names created online.
  */
 + (void)viewedLink:(NSString *)linkName;
 
 /**
- * Alert Yozio that a user has clicked on a link.
- * To edit this linkName, go to the VIRAL section of the Yozio website to find the appropriate link name to track.
- * @param linkName  the name of the link. Must match one of the link names created online.
+ * Notify Yozio that a user has shared a link.
+ *
+ * @param linkName  The name of the viral tracking link.
+ *                  Must match one of the link names created online.
  */
 + (void)sharedLink:(NSString *)linkName;
 
