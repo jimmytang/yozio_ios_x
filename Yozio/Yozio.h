29b5601ed04423770c8a5598d1bb306fa416bb2e
Jimmy Tang <jimmytang@gmail.com>
2012-12-31 10:39:52 -0800

    updating the Yozio SDK public methods

--- Yozio/Yozio.h
+++ Yozio/Yozio.h
@@ -28,49 +28,67 @@
 + (void)configure:(NSString *)appKey secretKey:(NSString *)secretKey callback:(void(^)(NSDictionary *))callback;
 
 /**
- * Makes a blocking HTTP request to download the experiment configurations.
- * Must be called prior to using any experiment related SDK calls
- * (i.e. stringForKey and intForKey).
+ * getYozioLink - Makes an blocking HTTP request to generate a Yozio link.
+ * getYozioLinkAsync - Makes an asynchronous HTTP request to generate a Yozio link.
+ *
+ * @param viralLoopName  Name of the viral loop. Must match the name of one of
+ *                       the viral loops created on the Yozio dashboard.
+ * @param channel  The social channel being used. Must match the channels
+                   selected for the viral loop created on Yozio dashboard.
+ * @param destinationUrl  URL that the generated Yozio link will redirect to.
+ * @param iosDestinationUrl  URL that the generated Yozio link will redirect to
+ *                           for iOS devices.
+ * @param androidDestinationUrl URL that the generated Yozio link will redirect
+ *                              to for Android devices.
+ * @param nonMobileDestinationUrl  URL that the generated Yozio link will
+ *                                 redirect to for all other devices.
+ * @param properties  Arbitrary meta data to attach to the generated Yozio link.
+ * @param callback  Called when the HTTP request completes.
+ *                  The argument passed into the callback will be the Yozio
+ *                  link, or the nonMobileDestinationUrl if there is an error
+ *                  generating the Yozio link.
  */
-+ (void)initializeExperiments;
++ (NSString *)getYozioLink:(NSString *)viralLoopName
+                   channel:(NSString *)channel
+            destinationUrl:(NSString *)destinationUrl;
++ (NSString *)getYozioLink:(NSString *)viralLoopName
+                   channel:(NSString *)channel
+            destinationUrl:(NSString *)destinationUrl
+                properties:(NSDictionary *)properties;
++ (void)getYozioLinkAsync:(NSString *)viralLoopName
+                  channel:(NSString *)channel
+           destinationUrl:(NSString *)destinationUrl
+                 callback:(void(^)(NSString *))callback;
++ (void)getYozioLinkAsync:(NSString *)viralLoopName
+                  channel:(NSString *)channel
+           destinationUrl:(NSString *)destinationUrl
+               properties:(NSDictionary *)properties
+                 callback:(void(^)(NSString *))callback;
 
-/**
- * Makes an asynchronous HTTP request to download the experiment configurations.
- * Must be called prior to using any experiment related SDK calls
- * (i.e. stringForKey and intForKey).
- *
- * @param callback  Called when experiments has been initialized. 
- */
-+ (void)initializeExperimentsAsync:(void(^)(void))callback;
-
-/**
- * Retrieve the string value for a given configuration key.
- *
- * @param key  Key of the value to retrieve.
- * @param defaultValue  Value to return if the key is not found.
- * @return String value, or defaultValue if the key is not found.
- */
-+ (NSString*)stringForKey:(NSString *)key defaultValue:(NSString *)defaultValue;
-
-/**
- * Retrieve the integer value for a given configuration key.
- *
- * @param key  Key of the value to retrieve.
- * @param defaultValue  Value to return if the key is not found.
- * @return Integer value, or defaultValue if the key is not found.
- */
-+ (NSInteger)intForKey:(NSString *)key defaultValue:(NSInteger)defaultValue;
-
-/**
- * Notify Yozio that your user logged in.
- *
- * This will allow you to tie exported Yozio data with your own data.
- *
- * Warning: do not provide any personally identifiable information.
- *
- * @param userName  Name of the user that just logged in.
- */
-+ (void)userLoggedIn:(NSString *)userName;
++ (NSString *)getYozioLink:(NSString *)viralLoopName
+                   channel:(NSString *)channel
+         iosDestinationUrl:(NSString *)iosDestinationUrl
+     androidDestinationUrl:(NSString *)androidDestinationUrl
+   nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl;
++ (NSString *)getYozioLink:(NSString *)viralLoopName
+                   channel:(NSString *)channel
+         iosDestinationUrl:(NSString *)iosDestinationUrl
+     androidDestinationUrl:(NSString *)androidDestinationUrl
+   nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl
+                properties:(NSDictionary *)properties;
++ (void)getYozioLinkAsync:(NSString *)viralLoopName
+                  channel:(NSString *)channel
+        iosDestinationUrl:(NSString *)iosDestinationUrl
+    androidDestinationUrl:(NSString *)androidDestinationUrl
+  nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl
+                 callback:(void(^)(NSString *))callback;
++ (void)getYozioLinkAsync:(NSString *)viralLoopName
+                  channel:(NSString *)channel
+        iosDestinationUrl:(NSString *)iosDestinationUrl
+    androidDestinationUrl:(NSString *)androidDestinationUrl
+  nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl
+               properties:(NSDictionary *)properties
+                 callback:(void(^)(NSString *))callback;
 
 /**
  * Notify Yozio that your user logged in.
@@ -82,177 +100,10 @@
  * @param userName  Name of the user that just logged in.
  * @param properties  Arbitrary meta data to attach to this event.
  */
++ (void)userLoggedIn:(NSString *)userName;
 + (void)userLoggedIn:(NSString *)userName properties:(NSDictionary *)properties;
 
 /**
- * Makes a blocking HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param destinationUrl  URL that the generated Yozio link will redirect to.
- * @return A Yozio link, or the destinationUrl if there is an error generating
- *         the Yozio link.
- */
-+ (NSString *)getYozioLink:(NSString *)viralLoopName
-                   channel:(NSString *)channel
-            destinationUrl:(NSString *)destinationUrl;
-
-/**
- * Makes a blocking HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param destinationUrl  URL that the generated Yozio link will redirect to.
- * @param properties  Arbitrary meta data to attach to the generated Yozio link.
- * @return A Yozio link, or the destinationUrl if there is an error generating
- *         the Yozio link.
- */
-+ (NSString *)getYozioLink:(NSString *)viralLoopName
-                   channel:(NSString *)channel
-            destinationUrl:(NSString *)destinationUrl
-                properties:(NSDictionary *)properties;
-
-/**
- * Makes an asynchronous HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param destinationUrl  URL that the generated Yozio link will redirect to.
- * @param callback  Called when the HTTP request completes.
- *                  The argument passed into the callback will be the Yozio
- *                  link, or the destinationUrl if there is an error generating
- *                  the Yozio link.
- */
-+ (void)getYozioLinkAsync:(NSString *)viralLoopName
-                  channel:(NSString *)channel
-           destinationUrl:(NSString *)destinationUrl
-                 callback:(void(^)(NSString *))callback;
-
-/**
- * Makes an asynchronous HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param destinationUrl  URL that the generated Yozio link will redirect to.
- * @param properties  Arbitrary meta data to attach to the generated Yozio link.
- * @param callback  Called when the HTTP request completes.
- *                  The argument passed into the callback will be the Yozio
- *                  link, or the destinationUrl if there is an error generating
- *                  the Yozio link.
- */
-+ (void)getYozioLinkAsync:(NSString *)viralLoopName
-                  channel:(NSString *)channel
-           destinationUrl:(NSString *)destinationUrl
-               properties:(NSDictionary *)properties
-                 callback:(void(^)(NSString *))callback;
-
-/**
- * Makes a blocking HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param iosDestinationUrl  URL that the generated Yozio link will redirect to
- *                           for iOS devices.
- * @param androidDestinationUrl URL that the generated Yozio link will redirect
- *                              to for Android devices.
- * @param nonMobileDestinationUrl  URL that the generated Yozio link will
- *                                 redirect to for all other devices.
- * @return A Yozio link, or the nonMobileDestinationUrl if there is an error
- *         generating the Yozio link.
- */
-+     (NSString *)getYozioLink:(NSString *)viralLoopName
-                       channel:(NSString *)channel
-             iosDestinationUrl:(NSString *)iosDestinationUrl
-         androidDestinationUrl:(NSString *)androidDestinationUrl
-       nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl;
-
-/**
- * Makes a blocking HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param iosDestinationUrl  URL that the generated Yozio link will redirect to
- *                           for iOS devices.
- * @param androidDestinationUrl URL that the generated Yozio link will redirect
- *                              to for Android devices.
- * @param nonMobileDestinationUrl  URL that the generated Yozio link will
- *                                 redirect to for all other devices.
- * @param properties  Arbitrary meta data to attach to the generated Yozio link.
- * @return A Yozio link, or the nonMobileDestinationUrl if there is an error
- *         generating the Yozio link.
- */
-+     (NSString *)getYozioLink:(NSString *)viralLoopName
-                       channel:(NSString *)channel
-             iosDestinationUrl:(NSString *)iosDestinationUrl
-         androidDestinationUrl:(NSString *)androidDestinationUrl
-       nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl
-                    properties:(NSDictionary *)properties;
-
-/**
- * Makes an asynchronous HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param iosDestinationUrl  URL that the generated Yozio link will redirect to
- *                           for iOS devices.
- * @param androidDestinationUrl URL that the generated Yozio link will redirect
- *                              to for Android devices.
- * @param nonMobileDestinationUrl  URL that the generated Yozio link will
- *                                 redirect to for all other devices.
- * @param callback  Called when the HTTP request completes.
- *                  The argument passed into the callback will be the Yozio
- *                  link, or the nonMobileDestinationUrl if there is an error
- *                  generating the Yozio link.
- */
-+     (void)getYozioLinkAsync:(NSString *)viralLoopName
-                      channel:(NSString *)channel
-            iosDestinationUrl:(NSString *)iosDestinationUrl
-        androidDestinationUrl:(NSString *)androidDestinationUrl
-      nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl
-                     callback:(void(^)(NSString *))callback;
-
-/**
- * Makes an asynchronous HTTP request to generate a Yozio link.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param iosDestinationUrl  URL that the generated Yozio link will redirect to
- *                           for iOS devices.
- * @param androidDestinationUrl URL that the generated Yozio link will redirect
- *                              to for Android devices.
- * @param nonMobileDestinationUrl  URL that the generated Yozio link will
- *                                 redirect to for all other devices.
- * @param properties  Arbitrary meta data to attach to the generated Yozio link.
- * @param callback  Called when the HTTP request completes.
- *                  The argument passed into the callback will be the Yozio
- *                  link, or the nonMobileDestinationUrl if there is an error
- *                  generating the Yozio link.
- */
-+     (void)getYozioLinkAsync:(NSString *)viralLoopName
-                      channel:(NSString *)channel
-            iosDestinationUrl:(NSString *)iosDestinationUrl
-        androidDestinationUrl:(NSString *)androidDestinationUrl
-      nonMobileDestinationUrl:(NSString *)nonMobileDestinationUrl
-                   properties:(NSDictionary *)properties
-                     callback:(void(^)(NSString *))callback;
-
-/**
  * Notify Yozio that the user has entered the viral loop.
  *
  * This event should be triggered at whatever point you define the beginning
@@ -263,68 +114,12 @@
  *
  * @param viralLoopName  Name of the viral loop. Must match the name of one of
  *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels 
+ * @param channel  The social channel being used. Must match the channels
                    selected for the viral loop created on Yozio dashboard.
+ * @param properties  Arbitrary meta data to attach to the event.
  */
 + (void)enteredViralLoop:(NSString *)viralLoopName channel:(NSString *)channel;
-
-/**
- * Notify Yozio that the user has entered the viral loop.
- *
- * This event should be triggered at whatever point you define the beginning
- * of the viral loop to be.
- *
- * For example, a user can enter a viral loop whenever the share button for
- * the viral loop is shown.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
-                   selected for the viral loop created on Yozio dashboard.
- * @param properties  Arbitrary meta data to attach to the event.
- */
 + (void)enteredViralLoop:(NSString *)viralLoopName channel:(NSString *)channel properties:(NSDictionary *)properties;
-
-/**
- * Notify Yozio that the user has shared a Yozio link.
- *
- * This event should be triggered whenever a user has successfully shared a
- * Yozio link generated by getYozioLink.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
- selected for the viral loop created on Yozio dashboard.
- */
-+ (void)sharedYozioLink:(NSString *)viralLoopName channel:(NSString *)channel;
-
-/**
- * Notify Yozio that the user has shared a Yozio link.
- *
- * This event should be triggered whenever a user has successfully shared a
- * Yozio link generated by getYozioLink.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
- selected for the viral loop created on Yozio dashboard.
- * @param count  The number of shares this sharing event is creating. Example is a text message sent to 5 people should have a count of 5.
- */
-+ (void)sharedYozioLink:(NSString *)viralLoopName channel:(NSString *)channel count:(NSInteger)count;
-
-/**
- * Notify Yozio that the user has shared a Yozio link.
- *
- * This event should be triggered whenever a user has successfully shared a
- * Yozio link generated by getYozioLink.
- *
- * @param viralLoopName  Name of the viral loop. Must match the name of one of
- *                       the viral loops created on the Yozio dashboard.
- * @param channel  The social channel being used. Must match the channels
- selected for the viral loop created on Yozio dashboard.
- * @param properties  Arbitrary meta data to attach to the event.
- */
-+ (void)sharedYozioLink:(NSString *)viralLoopName channel:(NSString *)channel properties:(NSDictionary *)properties;
 
 /**
  * Notify Yozio that the user has shared a Yozio link.
@@ -339,6 +134,9 @@
  * @param count  The number of shares this sharing event is creating.
  * @param properties  Arbitrary meta data to attach to the event. Example is a text message sent to 5 people should have a count of 5.
  */
++ (void)sharedYozioLink:(NSString *)viralLoopName channel:(NSString *)channel;
++ (void)sharedYozioLink:(NSString *)viralLoopName channel:(NSString *)channel count:(NSInteger)count;
++ (void)sharedYozioLink:(NSString *)viralLoopName channel:(NSString *)channel properties:(NSDictionary *)properties;
 + (void)sharedYozioLink:(NSString *)viralLoopName
                 channel:(NSString *)channel
                   count:(NSInteger)count
