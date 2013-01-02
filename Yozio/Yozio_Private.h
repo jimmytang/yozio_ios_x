a4e831c1aebea83594421d5daad1871df058c9bc
Jimmy Tang <jimmytang@gmail.com>
2013-01-01 22:17:26 -0800

    fixing up routes

--- Yozio/Yozio_Private.h
+++ Yozio/Yozio_Private.h
@@ -21,7 +21,7 @@
 #define YOZIO_GET_URL_ROUTE @"/api/viral/v1/get_url"
 #define YOZIO_OPENED_APP_ROUTE @"/api/sdk/v1/opened_app"
 #define YOZIO_BATCH_EVENTS_ROUTE @"/api/sdk/v1/batch_events"
-#define YOZIO_LAUNCH_APP @"/api/yozio/v1/launch_app"
+#define YOZIO_LAUNCH_APP @"/api/sdk/v1/launch_app"
 
 #define YOZIO_GET_CONFIGURATION_P_APP_KEY @"app_key"
 #define YOZIO_GET_CONFIGURATION_P_YOZIO_UDID @"yozio_udid"
